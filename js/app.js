b780f95f31e227ee5fe2e8ef84e53bdd35728eab
Manuel Aberg Cobo <manuel.aberg@globant.com>
2015-07-03 11:53:17 -0300

    add user story factory

--- js/app.js
+++ js/app.js
@@ -1,25 +1,47 @@
 /* global angular */
 'use strict';
-var module = angular.module('estimatio', ['angularGrid']);
+var module = angular.module('estimation', ['angularGrid']);
 
-module.controller('estimatioCtrl', function($scope) {
+module.controller('estimationCtrl', function($scope) {
+  $scope.estimation = {};
+});
 
-  var columnDefs = [
-    {headerName: 'Make', field: 'make'},
-    {headerName: 'Model', field: 'model'},
-    {headerName: 'Price', field: 'price'}
+module.factory('userStoriesFactory', function() {
+  var userStories = [
+    {owner: 'Estimator 1', number: 'US-01', name: 'User Store 1', release: '1', details: 'As a user, I want to...', type: 'User Story'}
   ];
 
-  var rowData = [
-    {make: 'Toyota', model: 'Celica', price: 35000},
-    {make: 'Ford', model: 'Mondeo', price: 32000},
-    {make: 'Porsche', model: 'Boxter', price: 72000}
+  var add = function (userStory) {
+    userStories.push(userStory);
+  };
+
+  return {
+    userStories: userStories,
+    add: add
+  };
+});
+
+module.controller('userStoriesCtrl', function($scope, userStoriesFactory) {
+  var columnDefs = [
+    {headerName: 'Owner', field: 'owner'},
+    {headerName: '#', field: 'number'},
+    {headerName: 'Name', field: 'name'},
+    {headerName: 'Release', field: 'release'},
+    {headerName: 'Details', field: 'details'},
+    {headerName: 'Type (Epic, User Story, Technical Story)', field: 'type'}
   ];
 
   $scope.gridOptions = {
     columnDefs: columnDefs,
-    rowData: rowData,
+    rowData: userStoriesFactory.userStories,
     dontUseScrolls: true // because so little data, no need to use scroll bars
   };
 
+  $scope.userStory = {};
+
+  $scope.save = function(userStory) {
+    userStoriesFactory.add(userStory);
+    $scope.gridOptions.api.onNewRows();
+    $scope.userStory = {};
+  };
 });

08c03e146e9ab791b47a246679df316e75a4d9db
unknown <emiliano.pistone@GLB-L2926.globant.com>
2015-07-03 16:43:20 -0300

    add new columns to task grid fixed

--- js/app.js
+++ js/app.js
@@ -39,8 +39,9 @@ module.factory('tasksFactory', function(modifiers) {
     task.usRelease = task.userStory.release;
     task.unitTestingFront = Math.ceil((modifiers.unitTestingFront * task.frontend) / 100);
     task.unitTestingBack = Math.ceil((modifiers.unitTestingBack * task.backend) / 100);
+    task.issueFixingFront = Math.ceil((modifiers.issueFixingFront * task.frontend) / 100); 
     task.issueFixingBack = Math.ceil((modifiers.issueFixingBack * task.backend) / 100);
-    task.issueFixingFront = Math.ceil((modifiers.issueFixingFront * task.backend) / 100); 
+    
     tasks.push(task);
     __taskId++;
   };
@@ -49,8 +50,8 @@ module.factory('tasksFactory', function(modifiers) {
     var task = tasks.filter(function(x){return x.id === newValue.id;})[0];
     newValue.unitTestingFront = Math.ceil((modifiers.unitTestingFront * newValue.frontend) / 100);
     newValue.unitTestingBack = Math.ceil((modifiers.unitTestingBack * newValue.backend) / 100);
-    newValue.issueFixingBack = Math.ceil((modifiers.issueFixingBack * newValue.frontend) / 100);
-    newValue.issueFixingFront = Math.ceil((modifiers.issueFixingFront * newValue.backend) / 100); 
+    newValue.issueFixingFront = Math.ceil((modifiers.issueFixingFront * newValue.frontend) / 100); 
+    newValue.issueFixingBack = Math.ceil((modifiers.issueFixingBack * newValue.backend) / 100);
     tasks[tasks.indexOf(task)] = newValue;
   };
 

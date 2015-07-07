2e7908a21520f18068ec898aa6a1685a27108926
unknown <emiliano.pistone@GLB-L2926.globant.com>
2015-07-07 12:56:56 -0300

    Add the dinamic edition on the midifiers columns

--- js/controllers/profilesController.js
+++ js/controllers/profilesController.js
@@ -8,11 +8,18 @@ module.controller('profilesController', function($scope, profilesFactory) {
   var columnDefs = [
     {headerName: 'Name', field: 'name', editable: true},
     {headerName: 'Unit Testing', template: '<input type="checkbox" name="hasUnitTesting" ng-model="data.hasUnitTesting"/>', editable: true},
-    {headerName: 'Unit Testing Modifier', field: 'unitTestingModifier', editable: true},
+    {headerName: 'Unit Testing Modifier', field: 'unitTestingModifier', editable: enabledUnitTestingModifier},
     {headerName: 'Issue Fixing', template: '<input type="checkbox" name="hasIssueFixing" ng-model="data.hasIssueFixing"/>', editable: true},
-    {headerName: 'Issue Fixing Modifier', field: 'issueFixingModifier', editable: true},
+    {headerName: 'Issue Fixing Modifier', field: 'issueFixingModifier', editable: enabledIssueFixingModifier},
     {headerName: 'Manual Testing', template: '<input type="checkbox" name="hasManualTesting" ng-model="data.hasManualTesting"/>', editable: true},
   ];
+
+  function enabledUnitTestingModifier(data) {
+    return data.hasUnitTesting;
+  } 
+  function enabledIssueFixingModifier(data) {
+    return data.hasIssueFixing;
+  } 
 
   $scope.add = function (profile) {
     profilesFactory.add(profile);

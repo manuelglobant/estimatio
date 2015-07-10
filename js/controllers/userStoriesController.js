bd77439449c80036dbda37c7ccb8d94f1e0ccb52
unknown <emiliano.pistone@GLB-L2926.globant.com>
2015-07-10 14:40:44 -0300

    add new empty row functionality to UserStories

--- js/controllers/userStoriesController.js
+++ js/controllers/userStoriesController.js
@@ -5,12 +5,25 @@ var module = angular.module('userStoriesController', []);
 module.controller('userStoriesController', function($scope, userStoriesFactory) {
   var columnDefs = [
     {headerName: 'Owner', field: 'owner'},
-    {headerName: '#', field: 'number'},
-    {headerName: 'Name', field: 'name'},
-    {headerName: 'Release', field: 'release'},
-    {headerName: 'Details', field: 'details'},
-    {headerName: 'Type (Epic, User Story, Technical Story)', field: 'type'}
+    {headerName: '#', field: 'number', editable: true, newValueHandler: checkEmptyRow },
+    {headerName: 'Name', field: 'name', editable: true},
+    {headerName: 'Release', field: 'release', editable: true},
+    {headerName: 'Details', field: 'details', editable: true},
+    {headerName: 'Type (Epic, User Story, Technical Story)', field: 'type', editable: true}
   ];
+
+  function checkEmptyRow (newValue) {
+    if (newValue.data.emptyRow && newValue.newValue !== '') {
+      addEmptyRow();
+    }
+    newValue.data.emptyRow = false;
+    newValue.data[newValue.colDef.field] = newValue.newValue;
+    $scope.gridOptions.api.onNewRows();
+  }
+
+  function addEmptyRow () {
+    $scope.gridOptions.rowData.push({emptyRow : true, owner: '', number: '', name: '', release: '', details: '', type: ''});
+  }
 
   $scope.gridOptions = {
     columnDefs: columnDefs,
@@ -18,6 +31,8 @@ module.controller('userStoriesController', function($scope, userStoriesFactory) 
     enableColResize: true,
     ready: function(api) {
       api.sizeColumnsToFit();
+      addEmptyRow();
+      api.onNewRows();
     }
   };
 
--- js/factories/userStoriesFactory.js
+++ js/factories/userStoriesFactory.js
@@ -4,10 +4,10 @@ var module = angular.module('userStoriesFactory', []);
 
 module.factory('userStoriesFactory', function() {
   var userStories = [
-    {owner: 'Estimator 1', number: 'US-01', name: 'User Store 1', release: '1', details: 'As a user, I want to...', type: 'User Story'},
-    {owner: 'Estimator 1', number: 'US-02', name: 'User Store 1', release: '1', details: 'As a user, I want to...', type: 'User Story'},
-    {owner: 'Estimator 1', number: 'US-03', name: 'User Store 1', release: '1', details: 'As a user, I want to...', type: 'User Story'},
-    {owner: 'Estimator 1', number: 'US-04', name: 'User Store 1', release: '1', details: 'As a user, I want to...', type: 'User Story'}
+    {emptyRow: false, owner: 'Estimator 1', number: 'US-01', name: 'User Store 1', release: '1', details: 'As a user, I want to...', type: 'User Story'},
+    {emptyRow: false, owner: 'Estimator 1', number: 'US-02', name: 'User Store 1', release: '1', details: 'As a user, I want to...', type: 'User Story'},
+    {emptyRow: false, owner: 'Estimator 1', number: 'US-03', name: 'User Store 1', release: '1', details: 'As a user, I want to...', type: 'User Story'},
+    {emptyRow: false, owner: 'Estimator 1', number: 'US-04', name: 'User Store 1', release: '1', details: 'As a user, I want to...', type: 'User Story'}
   ];
 
   var add = function (userStory) {
